771b425db3c8715d442b04bcc6545d132a7fc5c3
Pauli Perälä <perala.pauli@gmail.com>
2013-11-05 23:36:17 +0200

    indent

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,7 +1,7 @@
 class Survey < ActiveRecord::Base
 	has_many :questions, inverse_of: :survey
 	has_many :responses, inverse_of: :survey
-has_many :options, through: :questions
+	has_many :options, through: :questions
 	validates :title, length: {minimum:3}
 
 	accepts_nested_attributes_for :questions, allow_destroy: true
