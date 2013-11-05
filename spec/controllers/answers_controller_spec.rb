d826f42464c53cfec374cd5b39cf231a69927cfe
Pauli Perälä <perala.pauli@gmail.com>
2013-11-05 12:13:58 +0200

    angularilize survey form, nested questions

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -17,7 +17,7 @@ require 'spec_helper'
 # is no simpler way to get a handle on the object needed for the example.
 # Message expectations are only used when there is no simpler way to specify
 # that an instance is receiving a specific message.
-
+ 
 describe AnswersController do
 
   # This should return the minimal set of attributes required to create a valid
