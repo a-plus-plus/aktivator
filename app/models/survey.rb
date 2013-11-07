26570ae5dad7ebde94c888695e11f6adf39019ae
Jenna Lindh <jenna.lindh@luukku.com>
2013-11-07 14:04:37 +0200

    tests and validations for backend

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,7 +2,8 @@ class Survey < ActiveRecord::Base
 	has_many :questions, inverse_of: :survey
 	has_many :responses, inverse_of: :survey
 	has_many :options, through: :questions
-	validates :title, length: {minimum:3}
+	validates :title, presence: true
+	validates :title, length: {maximum: 256}
 
 	accepts_nested_attributes_for :questions, allow_destroy: true
 	accepts_nested_attributes_for :options, allow_destroy: true
--- spec/factories.rb
+++ spec/factories.rb
@@ -3,17 +3,22 @@ FactoryGirl.define do
 	factory :answer do
 		response
 		question
+		value "sopiva pituus"
 	end	
 
 	factory :option do
 		question
+		value "kiva value"
 	end
 
 	factory :question do
 		survey
+		title "kysymys?"
+		kind "Radiobutton"
 	end
 
 	factory :survey do
+		title "sopiva otsikko"
 	end
 
 	factory :response do 
