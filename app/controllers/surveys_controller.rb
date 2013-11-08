f31140d8ff3229b7934b4db19c11354a1e66a1ef
Pauli Perälä <perala.pauli@gmail.com>
2013-11-08 13:47:00 +0200

    cleaned old serialization

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -5,10 +5,6 @@ class SurveysController < ApplicationController
   # GET /surveys.json
   def index
     @surveys = Survey.all
-    respond_to do |format|
-      format.html
-      format.json { render json: @surveys }
-    end
   end
 
   # GET /surveys/1
