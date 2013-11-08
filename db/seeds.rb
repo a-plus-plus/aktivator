8b8e60261d62484c0b0adb6d5d715b80dbdc47be
Kari Korpinen <karikkorpinen@gmail.com>
2013-11-08 15:09:40 +0200

    seeds for survey and questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Survey.create([{ title: 'Äärettömän monta ratkaisua' ,status:'Finished'},{ title: 'Porrasmatriisit',status:'Published'},{ title: 'Sidottu',status:'Unpublished'},{ title: 'Redusoidut porrasmatriisit',status:'Finished'},{ title: 'Virittämisväitteet',status:'Published'},{ title: 'Onko R^2 avaruuden R^3 aliavaruus?',status:'Finished'},{ title: 'Matriisiväitteet',status:'Unpublished'},{ title: 'Vapauskysymys',status:'Published'}])
+#questions
+Question.create([{ title: 'en tiedä ',survey_id: 1, kind: 'Radiobutton'}])
+Question.create([{ title: 'muu vastaus ',survey_id: 1, kind: 'Radiobutton'}])
+Question.create([{ title: ' A ja B',survey_id: 1, kind: 'Radiobutton'}])
+
+Question.create([{ title: 'En tiedä',survey_id: 6, kind: 'Radiobutton'}])
+Question.create([{ title: 'Ei',survey_id: 6, kind: 'Radiobutton'}])
+Question.create([{ title: 'Kyllä',survey_id: 6, kind: 'Radiobutton'}])
+
+Question.create([{ title: 'Tiedän',survey_id: 7, kind: 'Checkbox'}])
+Question.create([{ title: 'En tiedä',survey_id: 7, kind: 'Checkbox'}])
+Question.create([{ title: 'A',survey_id: 7, kind: 'Radiobutton'}])
+Question.create([{ title: 'B',survey_id: 7, kind: 'Radiobutton'}])
+Question.create([{ title: 'Molemmat',survey_id: 7, kind: 'Radiobutton'}])
+
+Question.create([{ title: 'En tiedä',survey_id: 8, kind: 'Radiobutton'}])
+Question.create([{ title: 'Olen samaa mieltä',survey_id: 8, kind: 'Radiobutton'}])
+Question.create([{ title: 'Vihje:ajattele eri vapausasteita',survey_id: 8, kind: 'Textfield'}])
