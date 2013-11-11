e5c62adbf50196fe8fa70a6eba3f8fd379e4da0c
Ronnie Brandtberg <r.brandtberg@gmail.com>
2013-11-11 11:54:59 +0200

    Tests and validations added for Tag and Survey

--- db/schema.rb
+++ db/schema.rb
@@ -63,6 +63,13 @@ ActiveRecord::Schema.define(version: 20131111091542) do
     t.integer "tag_id"
   end
 
+  create_table "taggings", force: true do |t|
+    t.integer  "survey_id"
+    t.integer  "tag_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "tags", force: true do |t|
     t.string   "title"
     t.datetime "created_at"
