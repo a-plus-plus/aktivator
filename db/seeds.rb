cb2a9444f807c05c25755c0520c4b58134bc7802
Jenna Lindh <jenna.lindh@luukku.com>
2013-11-11 13:23:17 +0200

    new update to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,11 +13,18 @@ u.surveys.create title: "LaMa", status: "Published"
 u.surveys.create title: "Lapio", status: "Finished"
 
 q = s.questions.create title: "Kuka?", kind: "Radiobutton"
-s.questions.create title: "Mikä?", kind: "Checkbox"
-s.questions.create title: "Missä?", kind: "Textfield"
+q.options.create value: "Heikki"
+q.options.create value: "Risto"
+q.options.create value: "Joku muu"
 
-q.options.create value: "Minä"
-q.options.create value: "joku muu"
+q = s.questions.create title: "Mikä?", kind: "Checkbox"
+q.options.create value: "Myyrä"
+q.options.create value: "Näätä"
+q.options.create value: "Siiseli"
+
+q = s.questions.create title: "Missä?", kind: "Textfield"
+
+
 
 #Survey.create([{ title: 'Äärettömän monta ratkaisua' ,status:'Finished'},{ title: 'Porrasmatriisit',status:'Published'},{ title: 'Sidottu',status:'Unpublished'},{ title: 'Redusoidut porrasmatriisit',status:'Finished'},{ title: 'Virittämisväitteet',status:'Published'},{ title: 'Onko R^2 avaruuden R^3 aliavaruus?',status:'Finished'},{ title: 'Matriisiväitteet',status:'Unpublished'},{ title: 'Vapauskysymys',status:'Published'}])
 ##questions
