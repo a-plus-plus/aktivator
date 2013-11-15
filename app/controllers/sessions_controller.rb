aa18923796f18011876f81c0c131f50af4a62dc9
Mikael <mikael.snellman@cs.helsinki.fi>
2013-11-15 16:41:37 +0200

    login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < Devise::SessionsController
+  #skip_before_filter :authenticate_user_from_token!, :only => [:create]
+  #skip_after_filter  :verify_authorized
+  
+  def create
+    resource = User.find_for_database_authentication(name: params[:user][:name])
+    return failure unless resource
+    return failure unless resource.valid_password?(params[:user][:password])
+    handle_success(resource)
+  end
+ 
+  def failure
+    render :nothing => true, :status => :unauthorized
+  end
+
+  def destroy
+    current_user.logout
+    render :json => {:success => true, :message => "logged out"}
+  end
+
+  private
+  def handle_success(resource)
+    token = resource.ensure_authentication_token
+    name = AES.encrypt(resource.name, Aktivator::Application.config.secret_key_base)
+    render :json => {:success => true, :name => name, :token => token}
+  end	
+end
