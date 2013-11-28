a24892e70b87b5f56ff809691d03dbc3849f84cd
Pauli Perälä <perala.pauli@gmail.com>
2013-11-28 16:09:14 +0200

    removed respond_to's
    we are only answering to json, so don’t allow anything else

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -30,34 +30,29 @@ class ResponsesController < ApplicationController
 
   # PATCH/PUT /responses/1{.json}
   def update
-    respond_to do |format|
-      if @response.update(response_params)
-        format.html { redirect_to @response, notice: 'Response was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @response.errors, status: :unprocessable_entity }
-      end
+    if @response.update(response_params)
+      head :no_content 
+    else
+      render json: @response.errors, status: :unprocessable_entity 
     end
   end
+end
 
   # DELETE /responses/1{.json}
   def destroy
     @response.destroy
-    respond_to do |format|
-      format.html { redirect_to responses_url }
-      format.json { head :no_content }
-    end
+    head :no_content 
+  end
+end
+
+private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_response
+    @response = Response.find(params[:id])
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_response
-      #@response = Response.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def response_params
-      params.require(:response).permit(:survey_id, answers_attributes:[:option_id,:value,:question_id])
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def response_params
+    params.require(:response).permit(:survey_id, answers_attributes:[:option_id,:value,:question_id])
+  end
   end
